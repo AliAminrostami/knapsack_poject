d4835cd6e9363bb90dc447411d8ddb8c1bdb8cf9
AliAminrostami <ali.aar1379@gmail.com>
2024-11-13 14:02:46 +0330

    Add files via upload

---
+++ heuristic.py
@@ -0,0 +1,128 @@
+# Importing Dependencies
+import pyomo.environ as pyo
+from pyomo.opt import SolverStatus, TerminationCondition
+import numpy as np
+import pandas as pd
+import time
+from datetime import datetime
+
+class Knapsack_Heuristic:
+
+    def __init__(self, LP_penaltiy_rate, LP_max_percentage, LP_min_value, nI, nK , Profits, Weights, Capacity , cpu_time_limit):
+        
+        self.PenaltiyRate = LP_penaltiy_rate
+        self.MaxLpperc = LP_max_percentage
+        self.MinValue = LP_min_value
+        self.nI = nI   # number of items
+        self.nK = nK   # number of knapstacks
+        self.benefit = Profits
+        self.weight = Weights
+        self.capacity = Capacity
+        self.cpu_time_limit = cpu_time_limit
+        
+    def optimize(self):  
+
+        # Model Definition
+        model = pyo.ConcreteModel()    
+
+        # Set Definition
+        model.N = pyo.RangeSet(1, self.nI) # RangeSet Items
+        model.M = pyo.RangeSet(1, self.nK) # RangeSate Knapsack
+
+        # Parameter Declarations
+        def c_init(model, j):
+            return self.benefit[j-1] 
+        model.c = pyo.Param(model.N, initialize = c_init, mutable = True) # Benefit 
+
+        def a_init(model, i, j):
+            return self.weight[i-1][j-1]
+        model.a = pyo.Param(model.M, model.N, initialize = a_init) # Weight 
+
+        def b_init(model, i):
+            return self.capacity[i-1]
+        model.b = pyo.Param(model.M, initialize = b_init) # Capacity
+
+        # Variable Declarations
+        model.x = pyo.Var(model.N, bounds = (0,1), domain = pyo.NonNegativeReals) # Lp
+
+        # Objective Function Definition
+        def obj_rule(model):
+            return sum(model.c[i] * model.x[i] for i in model.N)
+        model.obj = pyo.Objective(rule = obj_rule, sense = pyo.maximize)
+
+        # Constraint Declarations 
+        def con_rule(model, m):
+            return sum(model.a[m,i] * model.x[i] for i in model.N) <= model.b[m]
+        model.con = pyo.Constraint(model.M, rule = con_rule)
+
+        # creat the solver
+        solver = pyo.SolverFactory('cplex')
+        # solver.solve(model)
+ 
+        # Selected items function
+        def selection():
+            """ doc string """ 
+            s1 = []
+            for i in model.N:
+                if pyo.value(model.x[i]) > self.MinValue:
+                    s1.append(i)
+            return set(s1)
+    
+        selectedItems = set() # At the current iteration.
+        ItemsPool = set() # At the union of all iterations.
+        
+        start_time = time.time() # the start time of algorithm
+        
+        elapsed_time = 0
+
+        # Phase1
+        # Solve the penalized LP relaxation
+        #iteration = 1
+        
+        while((len(ItemsPool)/self.nI) < self.MaxLpperc and elapsed_time<self.cpu_time_limit):
+            
+            
+            results = solver.solve(model) 
+            selectedItems = selection() 
+            ItemsPool = ItemsPool.union(selectedItems)
+            for item in selectedItems: 
+                model.c[item] *= self.PenaltiyRate
+                
+        end_time = time.time()    
+        elapsed_time = end_time - start_time  # calculating the total time
+            
+        optimal_solution = int(pyo.value(model.obj))
+        solution_condition = results.solver.termination_condition
+            #iteration += 1
+        
+        ItemsPoolList = np.zeros(self.nI)
+        for item in ItemsPool: 
+            ItemsPoolList[item-1] = 1
+ 
+        
+        return ItemsPoolList, len(ItemsPool) , elapsed_time , optimal_solution , solution_condition
+    
+    def write_excel(self, LP_penaltiy_rate, LP_max_percentage, LP_min_value, len_ItemsPool , run_id , category \
+                    , problem_num , run_number , elapsed_time , optimal_solution , solution_condition):
+        
+        file_path = 'input_output/output.xlsx'
+
+        df = pd.read_excel(file_path)
+        new_data = {'run_id': run_id,
+                    'category': category, 
+                    'problem_num': problem_num, 
+                    'run_number': run_number,                    
+                    'nK': self.nK,
+                    'nI': self.nI,
+                    'cpu_time_limit': self.cpu_time_limit, 
+                    'LP_penaltiy_rate': LP_penaltiy_rate,
+                    'LP_max_percentage': LP_max_percentage,
+                    'LP_min_value': LP_min_value,
+                    'LP_len_pool_items': len_ItemsPool,
+                    'elapsed_time': elapsed_time,
+                    'optimal_solution': optimal_solution,
+                    'solution_condition':solution_condition,
+                    }
+
+        df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+        df.to_excel(file_path, index=False)
---
+++ main.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import reading_mknapcb as reading_mknapcb
+import diagram
+from cabc import Classic_Artificial_Bee_Colony
+from heuristic import Knapsack_Heuristic
+
+excel_file_path = 'input_output/input.xlsx'
+df = pd.read_excel(excel_file_path)
+
+for index, row in df.iloc[:].iterrows():
+
+    #0-run_id, 1-category, 2-problem_num, 3-run_number, 4-cpu_time_limit, 5-LP_penaltiy_rate, 6-LP_max_percentage, 7-LP_min_value
+    #8-ABC_bees_num, 9-ABC_max_try_improve, 10-ABC_onlooker_selection, 11-ABC_onlooker_KT, 12-ABC_cross_over_type, 13-ABC_pc_onePoint
+    #14-ABC_pc_uniForm, 15-ABC_pm
+
+    row_values = list(row.iloc)
+
+    run_id, category, problem_num, run_number, cpu_time_limit, \
+    LP_penaltiy_rate, LP_max_percentage, LP_min_value, \
+    ABC_employed_bees_num, ABC_max_try_improve, ABC_onlooker_selection, ABC_onlooker_KT, ABC_crossover_type, ABC_pc_onePoint, \
+    ABC_pc_uniForm, ABC_pm = \
+        row_values[0], row_values[1], row_values[2], row_values[3], row_values[4], row_values[5], row_values[6], row_values[7], \
+        row_values[8], row_values[9], row_values[10], row_values[11], row_values[12], row_values[13], row_values[14], row_values[15]
+
+
+    # nK = number of knapstacks
+    # nI = number of items
+    nK, nI, Capacity, Profits, Weights = reading_mknapcb.reading(category, problem_num)
+
+    heu = Knapsack_Heuristic(LP_penaltiy_rate, LP_max_percentage, LP_min_value, nI, nK, Profits, Weights, Capacity , cpu_time_limit)
+
+    # getting result
+    ItemsPoolList, len_ItemsPool , elapsed_time , optimal_solution , solution_condition = heu.optimize()
+    print(ItemsPoolList)
+    print(len_ItemsPool)
+    print(elapsed_time)
+    print(optimal_solution)
+    print(solution_condition)
+    
+    # write in excel_1
+    #heu.write_excel ( LP_penaltiy_rate, LP_max_percentage, LP_min_value, len_ItemsPool , run_id , category \
+       #             , problem_num , run_number , elapsed_time , optimal_solution , solution_condition)
+        
+    #result_file_name = f'input_output/{run_id}.txt'
+    #result = open(result_file_name, 'a')
+    #result.write(f"Hybrid LP Relaxation Artificial Bee Colony Algorithm on Multidimentional Knapsack Problem \n \n")        
+    #result.close()
+
+    #ABC_onlooker_bees_num = ABC_employed_bees_num
+    
+    # creating an abc object
+    #abc = Classic_Artificial_Bee_Colony(\
+       # run_id, category, problem_num, run_number, cpu_time_limit, \
+        #nK, nI, Capacity, Profits, Weights, \
+       # ABC_employed_bees_num, ABC_onlooker_bees_num, ABC_max_try_improve, ABC_onlooker_selection, ABC_onlooker_KT, \
+       # ABC_crossover_type, ABC_pc_onePoint, ABC_pc_uniForm, ABC_pm, result_file_name, ItemsPoolList)
+
+    # getting result
+   # len_bee, best_final_bee, best_final_fitness, best_fitnesses_of_iterations, best_fitnesses_so_far, best_fitness_iteration_num, \
+    #total_iteration_num, best_fitness_time, time_number_list = abc.optimize()
+
+    # writing the result in txt
+    #abc.write_results(LP_penaltiy_rate, LP_max_percentage, LP_min_value, len_ItemsPool, \
+        #len_bee, best_final_bee, best_final_fitness, best_fitnesses_of_iterations, best_fitness_iteration_num, total_iteration_num, \
+       # best_fitness_time)
+
+    # writing in excel 
+        #abc.write_excel(LP_penaltiy_rate, LP_max_percentage, LP_min_value, len_ItemsPool, \
+        #len_bee, best_final_fitness, best_fitness_iteration_num, total_iteration_num, best_fitness_time)
+        
+   # photo = f"input_output/{run_id}"
+    #diagram.diagram(best_fitnesses_of_iterations, best_fitnesses_so_far, time_number_list, photo)
+
+# End of program ------------------------ 
+
